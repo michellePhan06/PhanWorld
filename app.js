7d0bb584f3b31488aa8156c32a31a12bcc834f7b
michellePhan06 <mtp7872@mavs.uta.edu>
2025-12-19 04:21:02 -0600

    feat: add house icon button

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-// Register service worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
+// // Register service worker
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('service-worker.js');
+// }
+
+// // Example app logic CHANGE
+// document.getElementById('actionBtn').addEventListener('click', () => {
+//   alert('Your app is working!');
+// });
+
+// house click area logic
+const clickArea = document.getElementById("click-area");
+const popupIcon = document.getElementById("popup-icon");
+
+if (location.hostname !== 'localhost') {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js');
+  }
 }
 
-// Example app logic CHANGE
-document.getElementById('actionBtn').addEventListener('click', () => {
-  alert('Your app is working!');
+
+clickArea.addEventListener("click", () => {
+  popupIcon.style.display = "block";
+
+  setTimeout(() => {
+    popupIcon.style.display = "none";
+  }, 1000); // 1 second
 });
